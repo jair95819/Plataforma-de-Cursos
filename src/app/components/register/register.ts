d0a0a011437e279646f1c8e64ecede209543884e
jair <jairestebanca@gmail.com>
2025-12-02 13:34:57 -0500

    integracion con backend

--- src/app/components/register/register.ts
+++ src/app/components/register/register.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http'; // Importar HttpClient
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule, HttpClientModule],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
@@ -15,13 +16,32 @@ export class RegisterComponent {
   email: string = '';
   password: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient) {}
 
   onRegister() {
     if (this.nombre && this.email && this.password) {
-      console.log('Registro:', this.nombre, this.email);
-      alert('¡Registro exitoso! Bienvenido a CursoFinder');
-      this.router.navigate(['/dashboard']);
+      
+      // Creamos el objeto tal cual lo espera tu clase Java 'Usuario'
+      const usuarioBackend = {
+        nombre: this.nombre,
+        email: this.email,
+        contrasenaHash: this.password // Java espera 'contrasenaHash', Angular tiene 'password'
+      };
+
+      // Hacemos la petición POST al backend
+      this.http.post('http://localhost:8080/api/auth/registro', usuarioBackend)
+        .subscribe({
+          next: (response) => {
+            console.log('Usuario registrado:', response);
+            alert('¡Registro exitoso! Bienvenido a CursoFinder');
+            this.router.navigate(['/login']); // Redirigir al login, no al dashboard directo
+          },
+          error: (error) => {
+            console.error('Error:', error);
+            alert('Error al registrar: ' + (error.error || 'Intenta nuevamente'));
+          }
+        });
+
     } else {
       alert('Por favor completa todos los campos');
     }
